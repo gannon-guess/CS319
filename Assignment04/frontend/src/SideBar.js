5b80750c61e8d32f863a244526af5671838cff03
GannonGuess <gannon.guess@gmail.com>
2024-11-23 19:39:33 -0600

    assignment4 commit

---
+++ Assignment04/frontend/src/SideBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Gannon Guess
+// gannon@iastate.edu
+// Boudhayan Chakraborty
+// bcb43@iastate.edu
+// November 23, 2024
+
+const Sidebar = () => {
+    return (
+        <div className="d-flex flex-column vh-100 p-3 bg-light" style={{ width: '250px' }}>
+            <h2 className="text-center">Navigation</h2>
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link text-dark">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/contacts" className="nav-link text-dark">View Contacts</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/add-contact" className="nav-link text-dark">Add Contact</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/deletecontact" className="nav-link text-dark">Delete Contact</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/searchContacts" className="nav-link text-dark">Search Contacts</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ Assignment04/frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js"
+
+// Gannon Guess
+// gannon@iastate.edu
+// Boudhayan Chakraborty
+// bcb43@iastate.edu
+// November 23, 2024
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <App />
+    </div>
+);
